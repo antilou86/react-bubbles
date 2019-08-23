fe13cd75712bac170af3cd253c5b83cc61aa0563
antilou86 <antilou86@gmail.com>
2019-08-23 12:04:25 -0400

    WIP: built out log in page using Formik, Yup for validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Login from "./components/Login";
+import FormikLogin from "./components/Login";
 import "./styles.scss";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={FormikLogin} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,50 @@
 import React from "react";
+import { withFormik, Form, Field, yupToFormErrors } from "formik";
+import * as Yup from "yup";
 import axios from 'axios';
 
-const Login = () => {
+const Login = ({ values, errors, touched }) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <Form>
+        <div>
+          <Field
+            type="text" name="username" placeholder="enter username..."
+          />
+          {touched.username && errors.username && <p>{errors.username}</p>}
+        </div>
+        <div>
+          <Field
+            type="password" name="password" placeholder="enter password..."
+          />
+          {touched.password && errors.password && <p>{errors.password}</p>}
+        </div>
+        <button>Kenny Log-in!</button>
+      </Form>
     </>
   );
 };
-
-export default Login;
+const FormikLogin = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || '',
+      password: password || ''
+    }
+  },
+  validationSchema: yupToFormErrors.object().shape({
+    username: Yup.string().required('username is a required field, bruh.'),
+    password: Yup.string().required('not getting very far without a password tho.')
+  }),
+  handleSubmit(values) {
+    axios.post('http://localhost:5000/api/login', values)
+      .then(res => {
+        console.log(res.data)
+        localStorage.setItem('token', res.data.token)
+      })
+      .catch(err => console.log('Danger Will Robinson: ,', err))
+  }
+})(Login)
+export default FormikLogin;
