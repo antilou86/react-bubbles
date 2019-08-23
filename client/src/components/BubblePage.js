536f5ae64c099de6715022655a4b717d18bf25df
antilou86 <antilou86@gmail.com>
2019-08-23 12:42:40 -0400

    WIP: added fetch functionality, axios with auth fetches the data and sets it to the colorlist variable.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import {axiosWithAuth} from "../utils/axiosWithAuth"
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
@@ -9,7 +8,15 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
-  //useEffect(()=>{},[])
+ useEffect(()=>{
+   axiosWithAuth
+    .get('http://localhost:5000/api/colors')
+    .then(res => {
+      console.log(res.data);
+      setColorList(res.data.colors);
+    })
+    .catch(err => console.log('could not fetch colors, something is up: ', err))
+ },[colorList.length])
 
   return (
     <>
