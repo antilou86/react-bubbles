fc9c324270268dbcd09bca021135343f4907499e
antilou86 <antilou86@gmail.com>
2019-08-23 13:12:34 -0400

    WIP: added saveEdit and Delete calls with axiosWithAuth. testing now.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +21,25 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is it saved right now?
+    axiosWithAuth
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data.colors);
+        let color = res.data.color;
+        updateColors({ ...colors, color });
+      })
+      .catch()
   };
 
   const deleteColor = color => {
     // make a delete request to delete a color
+    axiosWithAuth
+      .put(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log('results from delete request,', res.data.colors)
+        updateColors(res.data.colors);
+      })
+      .catch(err => console.log('problem deleting- check it, ', err))
   };
 
   return (
